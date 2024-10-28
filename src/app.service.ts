547569c9ba276c6599efa1fd9b29feafcd6db711
hechunhui <499954367@qq.com>
2024-10-28 14:48:47 +0800

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!-test-ci';
+    return 'Hello World!-test-ci-cd';
   }
 }
