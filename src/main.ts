cbb6cc558b08dae4bf6eebf0071cf6f828303f04
hechunhui <499954367@qq.com>
2024-10-29 15:18:54 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import {readFileSync} from 'node:fs'
+import { createServer } from 'http'
+import { ConfigService } from '@nestjs/config'
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+  const httpsOptions = {
+    key: readFileSync(configService.get<string>('SSL_KEY_PATH')),
+    cert: readFileSync(configService.get<string>('SSL_CERT_PATH')),
+  }
+  const httpsApp = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
+  await httpsApp.listen(process.env.PORT ?? 3000);
+  const httpApp = await NestFactory.create(AppModule);
+  await httpApp.init();
+  createServer(httpApp.getHttpAdapter().getInstance()).listen(3001);
 }
 bootstrap();
