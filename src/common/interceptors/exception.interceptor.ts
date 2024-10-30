2ba39b853e9055aa54562d253f8443ed80e46398
he.chunhui <499954367@qq.com>
2024-10-30 22:19:55 +0800

    add file

--- src/common/interceptors/exception.interceptor.ts
+++ src/common/interceptors/exception.interceptor.ts
@@ -1,37 +1,40 @@
 import {
-    Injectable,
-    NestInterceptor,
-    ExecutionContext,
-    CallHandler,
-    HttpException,
-    HttpStatus
-  } from '@nestjs/common'
-  import { Observable } from 'rxjs'
-  import { catchError } from 'rxjs/operators'
-  
-  @Injectable()
-  export class ExceptionInterceptor implements NestInterceptor {
-    intercept (context: ExecutionContext, next: CallHandler): Observable<any> {
-      return next.handle().pipe(
-        catchError((error) => {
-          let status = HttpStatus.INTERNAL_SERVER_ERROR
-          let message: any = 'Internal server error'
-  
-          // 判断是否为 HttpException
-          if (error instanceof HttpException) {
-            status = error.getStatus()
-            message = error.getResponse()
-          } else {
-            message = error.message || 'An unexpected error occurred'
-          }
-          // 返回自定义的错误响应结构
-          throw new HttpException({
-            status: false,
-            statusCode: status,
-            message
-          }, status)
-        })
-      )
-    }
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  HttpException,
+  HttpStatus,
+  Logger
+} from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { catchError } from 'rxjs/operators'
+
+@Injectable()
+export class ExceptionInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(ExceptionInterceptor.name)
+
+  intercept (context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError((error) => {
+        let status = HttpStatus.INTERNAL_SERVER_ERROR
+        let message: any = 'Internal server error'
+
+        // 判断是否为 HttpException
+        if (error instanceof HttpException) {
+          status = error.getStatus()
+          message = error.getResponse()
+        } else {
+          message = error.message || 'An unexpected error occurred'
+        }
+        this.logger.error(`Status: ${status}, Error Message: ${error.message}`, error.stack)
+        // 返回自定义的错误响应结构
+        throw new HttpException({
+          status: false,
+          statusCode: status,
+          message
+        }, status)
+      })
+    )
   }
-  
+}
--- src/common/interceptors/response-format.interceptor.ts
+++ src/common/interceptors/response-format.interceptor.ts
@@ -1,26 +1,25 @@
 import {
-    Injectable,
-    NestInterceptor,
-    ExecutionContext,
-    CallHandler
-  } from '@nestjs/common'
-  import { Observable } from 'rxjs'
-  import { map } from 'rxjs/operators'
-  
-  @Injectable()
-  export class ResponseFormatInterceptor implements NestInterceptor {
-    intercept (context: ExecutionContext, next: CallHandler): Observable<any> {
-      const response = context.switchToHttp().getResponse()
-      return next.handle().pipe(
-        map((data) => {
-          return {
-            status: true,
-            statusCode: response.statusCode, // 动态获取状态码
-            message: 'success',
-            data
-          }
-        })
-      )
-    }
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler
+} from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+
+@Injectable()
+export class ResponseFormatInterceptor implements NestInterceptor {
+  intercept (context: ExecutionContext, next: CallHandler): Observable<any> {
+    const response = context.switchToHttp().getResponse()
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          status: true,
+          statusCode: response.statusCode, // 动态获取状态码
+          message: 'success',
+          data
+        }
+      })
+    )
   }
-  
+}
