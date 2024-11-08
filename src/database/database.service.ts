1cb03db86b0442fbe3500077710e161feb5674e4
he.chunhui <499954367@qq.com>
2024-11-08 21:48:57 +0800

    fixed db

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -7,7 +7,7 @@ export class DatabaseService {
 
   constructor () {
     this.pool = mysql.createPool({
-      host: '127.0.0.1',
+      host: 'localhost',
       port: 3306,
       user: 'root',
       password: 'xtdgjhch1016',
