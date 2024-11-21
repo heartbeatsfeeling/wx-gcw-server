b53fc0c5e1c682c9716031f1a8bb275482fe449f
hechunhui <499954367@qq.com>
2024-11-21 14:08:33 +0800

    add

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv'
-dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` })
+dotenv.config()
 
 export const dbConfig = {
   host: process.env.DB_HOST,
