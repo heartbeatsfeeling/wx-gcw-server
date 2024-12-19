8d3108885c860237ef7753247f64bf4a04e58f96
hechunhui <499954367@qq.com>
2024-12-19 14:52:03 +0800

    fixed: 整理app文件格式

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,14 @@
-import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
-import { DatabaseService } from './database/database.service'
 
 @Controller()
 export class AppController {
   constructor (
-    private readonly appService: AppService,
-    private readonly databaseService: DatabaseService
+    private readonly appService: AppService
   ) {}
 
   @Get()
   getHello (): string {
     return this.appService.getHello()
-  }
-
-  @Post('updateDb')
-  @HttpCode(200)
-  async updateDb (
-    @Body('sql') sql: string,
-    @Body('params') params = []
-  ) {
-    return this.databaseService.query(sql, params)
   }
 }
--- types/request.d.ts
+++ types/request.d.ts
@@ -1,9 +1,15 @@
 import { Request } from 'express'
 
 export interface CustomRequest extends Request {
+  /**
+   * web端使用
+   */
   cookies: {
     token: string
   }
+  /**
+   * 微信小程序端使用
+   */
   headers: {
     token?: string
   }
